9005fe2e5568b3ecc56c0c50c495f95c353181ed
nayanmeher <nayanmeher16@gmail.com>
2025-03-25 11:32:24 +0530

    project update

--- components/Header/index.js
+++ components/Header/index.js
@@ -145,7 +145,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:nayanmeher16@gmail.com")}>
               Contact
             </Button>
             {mounted && theme && data.darkMode && (
