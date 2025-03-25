f499cbd18ed679c7215060780020f0e0938c6d3d
nayanmeher <nayanmeher16@gmail.com>
2025-03-25 17:12:26 +0530

    final changes1

--- components/Header/index.js
+++ components/Header/index.js
@@ -77,7 +77,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   {showResume && (
                     <Button
                       onClick={() =>
-                        window.open("mailto:hello@chetanverma.com")
+                        window.open("mailto:nayanmeher16@gmail.com")
                       }
                     >
                       Resume
@@ -85,7 +85,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:nayanmeher16@gmail.com")}
                   >
                     Contact
                   </Button>
@@ -174,7 +174,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:nayanmeher16@gmail.com")}>
               Contact
             </Button>
 
